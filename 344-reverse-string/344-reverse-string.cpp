62b640918a4084303736d0053ec5748ae25007c4
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-04-01 14:14:40 +0530

    Time: 63 ms (5.43%), Space: 23.2 MB (41.72%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int low=0,high=s.size()-1;
+        while(low<high){
+            swap(s[low++],s[high--]);
+        }
+    }
+};
