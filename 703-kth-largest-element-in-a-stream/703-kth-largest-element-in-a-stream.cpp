a6e79dea1acc9f1dbe642c4d2b5aec337011b763
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-04-08 15:27:31 +0530

    Time: 28 ms (97.31%), Space: 19.8 MB (63.09%) - LeetHub

---
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,30 @@
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>>pq;
+    int K;
+    KthLargest(int k, vector<int>& nums) {
+        K=k;
+        for(int i=0;i<nums.size();i++){
+        if(pq.size()<k)pq.push(nums[i]);
+        else 
+        {
+            if(pq.top()<nums[i]){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        }
+    }
+    
+    int add(int val) {
+        if(pq.size()<K)pq.push(val);
+        else 
+        {
+            if(pq.top()<val){
+                pq.pop();
+                pq.push(val);
+            }
+        }
+        return pq.top();
+    }
+};
