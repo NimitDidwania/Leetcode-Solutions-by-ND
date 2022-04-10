69dc02e71a3e7f5dffd8195c01764bef632fcb53
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-04-10 20:48:49 +0530

    Time: 60 ms (84.79%), Space: 12.6 MB (19.09%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int fun(int ind,int n,vector<unordered_set<int>>&v,unordered_set<int>&used,vector<int>&curr){
+    int fun(int ind,int n,vector<unordered_set<int>>&v,unordered_set<int>&used){
         if(ind==1){return 1;}
         int ct=0;
         for(auto x:v[ind]){
             if(used.find(x)==used.end()){
                used.insert(x);
-                curr.push_back(x);
-               ct+=fun(ind-1,n,v,used,curr);
+               ct+=fun(ind-1,n,v,used);
                used.erase(x); 
-                curr.pop_back();
             }
         }
         return ct;
@@ -23,11 +21,7 @@ public:
                 if(i%j==0 || j%i==0)v[i].insert(j);
             }
         }
-        // for(int i=1;i<=n;i++){
-        //     for(auto x:v[i])cout<<x<<" ";cout<<endl;
-        // }
         unordered_set<int>used;
-        vector<int>curr;
-        return fun(n,n,v,used,curr);
+        return fun(n,n,v,used);
     }
 };
