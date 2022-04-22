313eb1509232f83805ed0c250b2fc4b2a65c492f
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-04-22 16:08:17 +0530

    Time: 62 ms (23.33%), Space: 26.2 MB (7.19%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/386-lexicographical-numbers.cpp
@@ -1,11 +1,47 @@
+struct Node{
+    Node* links[10];
+    bool last=false;
+};
+
 class Solution {
 public:
+    void fun(Node *node,int &curr,vector<int>&ans){
+        if(node==NULL){
+           
+            return;
+        }
+        if(node->last){
+            ans.push_back(curr);
+        
+        }
+        for(int i=0;i<10;i++){
+            if(node->links[i]){
+                curr=curr*10 + i;
+                fun(node->links[i],curr,ans);
+                curr/=10;
+            }
+        }
+    }
+    
     vector<int> lexicalOrder(int n) {
-        vector<string>help;
-        for(int i=1;i<=n;i++)help.push_back(to_string(i));
-        sort(help.begin(),help.end());
+        Node *root = new Node();
+        for(int i=1;i<=n;i++){
+            string s = to_string(i);
+            Node *node=root;
+            for(auto ch:s){
+                if(node->links[ch-'0']==NULL){
+                    Node *temp = new Node();
+                    node->links[ch-'0']=temp;
+                }
+                node=node->links[ch-'0'];
+            }
+            node->last=true;
+        }
+        
+        Node *node=root;
+        int curr=0;
         vector<int>ans;
-        for(int i=0;i<n;i++)ans.push_back(stoi(help[i]));
+        fun(node,curr,ans);
         return ans;
     }
 };
