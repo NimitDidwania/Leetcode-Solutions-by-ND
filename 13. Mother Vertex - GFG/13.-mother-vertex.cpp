59f27b96bb7058127890f299520d872a23a90811
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-05-07 12:19:03 +0530

    Added solution - LeetHub

--- 13. Mother Vertex - GFG/13.-mother-vertex.cpp
+++ 13. Mother Vertex - GFG/13.-mother-vertex.cpp
@@ -9,24 +9,38 @@ class Solution
 {
     public:
     //Function to find a Mother Vertex in the Graph.
-    void dfs(int s,int V,vector<int> adj[],vector<bool>&visited){
+    void dfs(int s,int V,vector<int> adj[],bool visited[],stack<int>&st){
         visited[s]=1;
         for(auto child:adj[s]){
-            if(!visited[child])dfs(child,V,adj,visited);
+            if(!visited[child])dfs(child,V,adj,visited,st);
         }
+        st.push(s);
+    }
+    void dfs2(int s,int V,vector<int> adj[],bool visited[],int &count){
+        visited[s]=1;
+        count++;
+        for(auto child:adj[s]){
+            if(!visited[child])dfs2(child,V,adj,visited,count);
+        }
+        
     }
 	int findMotherVertex(int V, vector<int>adj[])
 	{
 	    // Code here
+	    int entry=0;
+	    bool visited[V]={false};stack<int>st;
 	    for(int i=0;i<V;i++){
-	        vector<bool>visited(V,false);
-	        dfs(i,V,adj,visited);
-	        int ct=0;
-	        for(int i=0;i<V;i++)if(visited[i]==0)ct++;
-	        if(ct==0)
-	        return i;
+	       // if(entry==1)return -1;
+	        if(!visited[i])
+	        dfs(i,V,adj,visited,st);
+	       // entry++;
 	    }
-	    return -1;
+	    for(int i=0;i<V;i++)visited[i]=false;
+	    int count=0;
+	    dfs2(st.top(),V,adj,visited,count);
+	    if(count==V)
+	    return st.top();
+	    else return -1;
 	}
 
 };
