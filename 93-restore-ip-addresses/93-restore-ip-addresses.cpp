76f44348b960cb71d774b1c0cee38d0639bbf840
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-05-09 19:14:18 +0530

    Time: 3 ms (67.52%), Space: 7 MB (20.78%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/93-restore-ip-addresses.cpp
@@ -1,63 +1,47 @@
 class Solution {
 public:
-    void fun(string &s,int ind,string &curr,vector<string>&ans,int parts){
-        
-        if(ind==s.size())
-        {
-            if(parts==4)
-            {
-                string temp=curr;
-                temp.pop_back();
-                ans.push_back(temp);
+    void fun(int ind,string &s,string &curr,vector<string>&ans,int dots){
+        int n=s.size();
+        if(ind==n){
+            if(dots==4){
+                curr.pop_back();
+                ans.push_back(curr);
+                curr.push_back('.');
             }
             return;
         }
-        
-        if(s[ind]=='0'){
-            curr.push_back('0');
-            curr.push_back('.');
-            fun(s,ind+1,curr,ans,parts+1);
-            curr.pop_back();
-            curr.pop_back();
-        }
-        else{
-            curr.push_back(s[ind]);
-            curr.push_back('.');
-            fun(s,ind+1,curr,ans,parts+1);
-            curr.pop_back();
-            curr.pop_back();
-            //condition
-            if(ind<s.size()-1)
-            {curr.push_back(s[ind]);
-            curr.push_back(s[ind+1]);
-            curr.push_back('.');
-            fun(s,ind+2,curr,ans,parts+1);
-            curr.pop_back();
-            curr.pop_back();
-            curr.pop_back();}
-            //condition
-            
-            if(ind<s.size()-2 ){
-                string temp=s.substr(ind,3);
-                int num=stoi(temp);
-                if(num<=255){
-            curr.push_back(s[ind]);
-            curr.push_back(s[ind+1]);
-            curr.push_back(s[ind+2]);
-            curr.push_back('.');
-            fun(s,ind+3,curr,ans,parts+1);
-            curr.pop_back();
-            curr.pop_back();
-            curr.pop_back();
-            curr.pop_back();}
+        curr.push_back(s[ind]);
+        curr.push_back('.');
+        fun(ind+1,s,curr,ans,dots+1);
+        curr.pop_back();
+        curr.pop_back();
+        if(s[ind]!='0'){
+            if(ind<n-1){
+                curr.push_back(s[ind]);
+                curr.push_back(s[ind+1]);
+                curr.push_back('.');
+                fun(ind+2,s,curr,ans,dots+1);
+                curr.pop_back();
+                curr.pop_back();
+                curr.pop_back();
+            }
+            if(ind<n-2){
+                string temp;
+                temp.push_back(s[ind]);temp.push_back(s[ind+1]);temp.push_back(s[ind+2]);
+                if(stoi(temp)<=255){
+                    curr+=temp;
+                    curr.push_back('.');
+                    fun(ind+3,s,curr,ans,dots+1);
+                    curr.pop_back();curr.pop_back();curr.pop_back();curr.pop_back();
+                }
             }
         }
     }
     
     vector<string> restoreIpAddresses(string s) {
         vector<string>ans;
-        string curr="";
-        fun(s,0,curr,ans,0);
+        string curr;
+        fun(0,s,curr,ans,0);
         return ans;
     }
 };
