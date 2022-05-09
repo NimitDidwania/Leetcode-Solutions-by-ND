7e9cef090ab79a3c8619710696d256982f72308e
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-05-09 18:54:11 +0530

    Time: 7 ms (81.59%), Space: 10.7 MB (88.94%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    void fun(vector<int>&candidates,int target,int i,vector<int>&curr,vector<vector<int>>&ans){
-        //base case
-        if(target<0)return;
-        if(target==0){ans.push_back(curr);return;}
-        if(i==candidates.size())return;
-        curr.push_back(candidates[i]);
-        fun(candidates,target-candidates[i],i,curr,ans);
-        curr.pop_back();
-        fun(candidates,target,i+1,curr,ans);
+    void fun(int ind,vector<int>&curr,vector<vector<int>>&ans,vector<int>&candidates,int target){
+        if(target == 0){
+            ans.push_back(curr);
+            return ;
+        }
+        if(ind == candidates.size()){
+            if(target==0){
+                ans.push_back(curr);
+            }
+            return;
+        }
+        if(target>=candidates[ind])
+        {
+            curr.push_back(candidates[ind]);
+            fun(ind,curr,ans,candidates,target-candidates[ind]);
+            curr.pop_back();
+        }
+        fun(ind+1,curr,ans,candidates,target);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>curr;
-        fun(candidates,target,0,curr,ans);
+        fun(0,curr,ans,candidates,target);
         return ans;
     }
 };
