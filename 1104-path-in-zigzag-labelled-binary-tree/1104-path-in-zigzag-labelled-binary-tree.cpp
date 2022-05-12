4f3844deb4541b9441e82e35d84b1450608cfed4
Nimit Didwania <70817318+NimitDidwania@users.noreply.github.com>
2022-05-12 14:42:41 +0530

    Time: 5 ms (17.29%), Space: 6.3 MB (65.21%) - LeetHub

---
+++ 1104-path-in-zigzag-labelled-binary-tree/1104-path-in-zigzag-labelled-binary-tree.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        vector<int>ans;
+        int level = log2(label);
+        int x = level;
+        ans.push_back(label);
+        while(level!=0){
+            int srNo;
+            int p = pow(2,level);
+            if(!(level&1)){
+                srNo = label - p ; 
+                int leftParent=0;
+                if(srNo!=0)
+                 leftParent = srNo/2;
+                int parentLevel = level-1;
+                int pMax = p -1;
+                label = pMax - leftParent;
+            }
+            else {
+                srNo = 2*p -label-1;
+                int leftParent=0;
+                if(srNo!=0)
+                 leftParent =srNo/2;
+                int parentMin = p/2;
+                label=parentMin + leftParent;
+            }
+            ans.push_back(label);
+            if(level ==x )cout<<srNo<<endl;
+            level--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
